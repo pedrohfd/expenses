fab9ea49192398c11dd228737a501dd521894d25
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-11-04 21:48:09 -0300

    fix: reinstall pin code lib

--- src/pages/PinSetup/index.tsx
+++ src/pages/PinSetup/index.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
-import CodeInput from 'react-native-confirmation-code-input'
+import { Text } from 'react-native'
+import { CodeField } from 'react-native-confirmation-code-field'
 
 import { Header } from '../../components/Header'
 import { colors } from '../../styles/colors'
@@ -7,21 +8,40 @@ import { Container, Title } from './styles'
 
 export function PinSetup() {
   const pinCodeRef = useRef(null)
-  const [onFullFill, setOnFullFill] = useState('')
+  const [code, setCode] = useState('')
 
-  function handleOnFullFill(code: string) {}
+  function handleOnFullFill(code: string) {
+    console.log(code)
+  }
 
   return (
     <Container>
       <Title>Vamos configurar o seu PIN</Title>
 
-      <CodeInput
-        ref={pinCodeRef}
-        onFulfill={(code: string) => handleOnFullFill(code)}
-        className='border-circle'
-        cellBorderWidth={4}
-        activeColor='#fff'
-        codeInputStyle={{ backgroundColor: '#000', color: '#ddd' }}
+      <CodeField
+        value={code}
+        onChangeText={code => setCode(code)}
+        cellCount={4}
+        rootStyle={{ marginTop: 20 }}
+        textContentType='oneTimeCode'
+        renderCell={({ index, symbol, isFocused }) => (
+          <Text
+            key={index}
+            style={{
+              width: 40,
+              height: 40,
+              // lineHeight: 38,
+              // fontSize: 24,
+              textAlign: 'center',
+              borderWidth: 2,
+              borderColor: '#fff',
+              borderRadius: 20,
+              // backgroundColor: '#fff',
+            }}
+          >
+            {symbol}
+          </Text>
+        )}
       />
     </Container>
   )
